c47313cbfde85b997c3951bd51a009c935165e24
Michal Mocny <mmocny@gmail.com>
2023-03-29 12:14:41 -0400

    Fix console error

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ const PRIORITY = ['user-blocking', 'user-visible', 'background'][1];
 const controller = new TaskController({ priority: PRIORITY });
 
 function markNeedsNextPaint() {
-  // We cannot programatically *increase* the priority of Rendering...
-  // but we can decrease the priority of all the tasks we control
-  if (location.search.length > 0) {
-    controller.setPriority('background');
-  }
+	// We cannot programatically *increase* the priority of Rendering...
+	// but we can decrease the priority of all the tasks we control
+	if (location.search.length > 0) {
+		controller.setPriority('background');
+	}
 }
 
 function markDoneNextPaint() {
-  controller.setPriority(PRIORITY);
+	controller.setPriority(PRIORITY);
 }
 
 /*
  * Block the main thread for `ms`
  */
 function block(ms) {
-  const target = performance.now() + ms;
-  while (performance.now() < target);
+	const target = performance.now() + ms;
+	while (performance.now() < target);
 }
 
 /*
@@ -32,27 +32,27 @@ function block(ms) {
  * These are all scheduled up front, in FIFO order
  */
 function createTasks(numTasks, cb) {
-  for (let i = 0; i < numTasks; i++) {
-    scheduler.postTask(cb, {
-        signal: controller.signal,
-      }
-    ).catch((ex) => { });
-  }
+	for (let i = 0; i < numTasks; i++) {
+		scheduler.postTask(cb, {
+				signal: controller.signal,
+			}
+		).catch((ex) => { });
+	}
 }
 
 /*
  * Start a rAF loop
  */
 function startRAFLoop(cb) {
-  let rafid = 0;
-  function raf() {
-    rafid = requestAnimationFrame(() => {
-      cb();
-      raf();
-    });
-  }
-  raf();
-  return () => cancelAnimationFrame(rafid);
+	let rafid = 0;
+	function raf() {
+		rafid = requestAnimationFrame(() => {
+			cb();
+			raf();
+		});
+	}
+	raf();
+	return () => cancelAnimationFrame(rafid);
 }
 
 /*
@@ -61,22 +61,22 @@ function startRAFLoop(cb) {
  * open -a /Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --args --force-enable-metrics-reporting --enable-blink-featutres=LongAnimationFrameTiming
  */
 function startMarkingLoAF() {
-  if (!PerformanceObserver.supportedEntryTypes.includes('long-animation-frame')) {
-    return console.warn(
-      'LoAF Entry type not supported.  Type launching Canary with --enable-blink-featutres=LongAnimationFrameTiming'
-    );
-  }
+	if (!PerformanceObserver.supportedEntryTypes.includes('long-animation-frame')) {
+		return console.warn(
+			'LoAF Entry type not supported.  Type launching Canary with --enable-blink-featutres=LongAnimationFrameTiming'
+		);
+	}
 
-  const observer = new PerformanceObserver((list) => {
-    for (let entry of list.getEntries()) {
-      console.log(entry);
-      performance.measure('LoAF', {
-        start: entry.startTime,
-        duration: entry.duration,
-      });
-    }
-  });
-  observer.observe({ type: 'long-animation-frame', buffered: true });
+	const observer = new PerformanceObserver((list) => {
+		for (let entry of list.getEntries()) {
+			console.log(entry);
+			performance.measure('LoAF', {
+				start: entry.startTime,
+				duration: entry.duration,
+			});
+		}
+	});
+	observer.observe({ type: 'long-animation-frame', buffered: true });
 }
 
 
@@ -85,40 +85,36 @@ function startMarkingLoAF() {
  * Lets get going!
  */
 function main() {
-  startMarkingLoAF();
+	startMarkingLoAF();
 
-  const start = performance.now();
+	const start = performance.now();
 
-  createTasks(1000, () => {
-    // Update the DOM
-    const now = performance.now()
-    timer.innerHTML = now.toFixed(0);
+	createTasks(1000, () => {
+		// Update the DOM
+		const now = performance.now()
+		timer.innerHTML = now.toFixed(0);
 
-    // Add a bit of work to the task
-    block(10);
+		// Add a bit of work to the task
+		block(10);
 
-    // If you "need a next paint" quickly...
-    markNeedsNextPaint();
-  });
+		// If you "need a next paint" quickly...
+		markNeedsNextPaint();
+	});
 
-  const stopRAFLoop = startRAFLoop(() => {
-    markDoneNextPaint();
-  });
+	const stopRAFLoop = startRAFLoop(() => {
+		markDoneNextPaint();
+	});
 
-  stopButton.addEventListener('click', () => {
-    stopRAFLoop();
-    controller.abort();
-  });
-
-  scheduler.postTask(() => stopTheDemo(stopRunLoop),{
-    priority: 'user-blocking',
-    delay: DEMO_LENGTH,
-  }
-);
+	const stopTheDemo = () => {
+		stopRAFLoop();
+		controller.abort();
+	};
+	stopButton.addEventListener('click', stopTheDemo);
+	scheduler.postTask(stopTheDemo);
 }
 
 setTimeout(() => {
-  main();
+	main();
 }, 1000);
 
 
